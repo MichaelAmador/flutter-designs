00052a485b21b1a58ceb196aa2f1d63b7c6715c3
Michael Amador Centeno <57235798+MichaelAmador@users.noreply.github.com>
2023-08-03 08:53:52 -0600

    Add charts

--- lib/src/screens/screens.dart
+++ lib/src/screens/screens.dart
@@ -4,6 +4,7 @@ export 'labs/circular_progress_screen.dart';
 export 'labs/slideshow_screen.dart';
 
 export 'animations_screen.dart';
+export 'charts_screen.dart';
 export 'circular_graphics_screen.dart';
 export 'emergency_screen.dart';
 export 'headers_screen.dart';
